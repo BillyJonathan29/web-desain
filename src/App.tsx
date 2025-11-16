0099b93b07635343ccecfab12bbdfe485579b525
BillyJonathan29 <billyjonathan048@gmail.com>
2025-11-16 17:24:56 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./component/Navbar";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navbar />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+        <nav className='bg-blue-500 w-full'>
+            <div className="container p-5">
+                <h1 className='text-2xl text-red-500 flex justify-center font-extrabold'>Test geus jalan ncan tailwind na</h1>
+            </div>
+        </nav>
+    </>
+  )
+}
+
+export default Navbar
